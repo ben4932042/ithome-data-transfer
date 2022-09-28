d06ec1305b45ae407bce24de3b1cac088038c822
Ben Liu <ben4932042@icloud.com>
2022-09-28 19:34:34 +0800

    fix: gds filter error

--- sql/overwrite_content_info_view_change.sql
+++ sql/overwrite_content_info_view_change.sql
@@ -3,13 +3,17 @@ INSERT INTO `ithome-jenkins-2022.ithome.content_info_view_change` (
     `ironman_id`,
     `article_id`,
     `view`,
-    `crawl_datetime`
+    `crawl_datetime`,
+    `latest_datetime`
 )
 SELECT
   `ironman_id`,
   `article_id`,
   `view`,
-  `crawl_datetime`
+  `crawl_datetime`,
+  CASE WHEN
+      DATE(crawl_datetime) = @execute_date THEN true ELSE false
+  END AS `latest_datetime`
 FROM
   `ithome-jenkins-2022.ithome.content_info_hist`
 WHERE DATE(crawl_datetime) <= @execute_date
